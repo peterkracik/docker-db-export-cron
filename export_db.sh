c57a577bf968ee3a72ab713120de78a55005151e
peter kracik <peter@kracik.sk>
2019-07-04 06:34:26 +0200

    logs

--- export_db.sh
+++ export_db.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-mysqldump -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} | gzip -c > ${EXPORT_PATH}/${MYSQL_DATABASE}-$(date +"%Y%m%d-%H%M%S").tar.gz
+echo "Backup of database ${MYSQL_DATABASE} from host ${MYSQL_HOST}"
+
+rm -f `ls ${EXPORT_PATH}/*  -t | awk 'NR>10'`
+
+$DATE=$(date +"%Y%m%d-%H%M%S")
+
+mysqldump -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} | gzip -c > ${EXPORT_PATH}/${MYSQL_DATABASE}-$DATE.tar.gz
+
+echo 'exported file : ${EXPORT_PATH}/${MYSQL_DATABASE}-$DATE.tar.gz'
