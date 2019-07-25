c0782b0d3074da4f3ec82589ceb651f5e1109216
peter kracik <peter@kracik.sk>
2019-07-25 07:33:03 +0200

    echo output

--- export_db.sh
+++ export_db.sh
@@ -8,4 +8,4 @@ DATE=`date +%Y%m%d-%H%M%S`
 
 mysqldump -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} | gzip -c > ${EXPORT_PATH}/${MYSQL_DATABASE}-$DATE.tar.gz
 
-echo 'exported file : ${EXPORT_PATH}/${MYSQL_DATABASE}-$DATE.tar.gz'
+echo "exported file : ${EXPORT_PATH}/${MYSQL_DATABASE}-$DATE.tar.gz"
